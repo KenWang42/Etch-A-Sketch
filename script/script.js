d481ce3187023fad822a46c048bc4f669477d537
KenWang42 <Kangyu.wang@gmail.com>
2019-09-18 17:04:16 +0800

    update: optional mode

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
 const container = document.getElementById('container');
 const ctn_width = 850;
 const ctn_height = 800;
+var size_cur = 20;
+var mode_cur = 'mono';
 
 function make_grid(size) {
+  size_cur = size;
   for (var i = 0; i < size * size; i++) {
     var div = document.createElement('div');
     div.className = "block";
@@ -27,10 +30,10 @@ function reset_grid() {
     size = 64;
   } 
   make_grid(size);
-  mono();
+  set_mode(mode_cur);
 }
 
-function mode_mono() {
+function set_mono() {
   var blocks = document.querySelectorAll('.block'), i;
   blocks.forEach(block => {
     block.addEventListener('mouseover', function(e){
@@ -39,14 +42,54 @@ function mode_mono() {
   })
 }
 
-function mode_rgb() {
-
+function set_rgb() {
+  var blocks = document.querySelectorAll('.block'), i;
+  blocks.forEach(block => {
+    block.addEventListener('mouseover', function(e){
+      var a = get_random_rgb(),
+          b = get_random_rgb(),
+          c = get_random_rgb();
+      e.target.style.background = 'rgb(' + a + ',' + b + ',' + c + ')';
+    })
+  })
 }
 
-function mode_grey() {
-  
+function get_random_rgb() {
+  return Math.floor(Math.random()*255);
+}
+
+function set_gray() {
+  var blocks = document.querySelectorAll('.block'), i;
+  blocks.forEach(block => {
+    block.style.opacity = 0;
+    block.addEventListener('mouseover', function(e){
+      let op = block.style.opacity;
+      if (op < 1) {
+        op = parseFloat(op) + 0.1;
+        block.style.opacity = op;
+      }
+    })
+  })
+}
+
+function set_mode(mode) {
+  make_grid(size_cur);
+  mode_cur = mode;
+  switch (mode) {
+    case "mono":
+      set_mono();
+      break;
+    case "rgb":
+      set_rgb();
+      break;
+    case "gray":
+      set_gray();
+      break;
+  }
+
+  clear_grid();
 }
 
 // initial calling
-make_grid(20);
-mono();
+make_grid(size_cur);
+set_mode(mode_cur);

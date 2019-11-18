850f8ce9350870effa6b40f6fe766ce00537ea27
KenWang42 <Kangyu.wang@gmail.com>
2019-11-18 18:14:37 +0800

    fixed bug: duplicate grids

--- script/script.js
+++ script/script.js
@@ -1,13 +1,13 @@
-const container = document.getElementById('container');
+const container = document.getElementById("container");
 const ctn_width = 850;
 const ctn_height = 800;
 var size_cur = 20;
-var mode_cur = 'mono';
+var mode_cur = "mono";
 
 function make_grid(size) {
   size_cur = size;
   for (var i = 0; i < size * size; i++) {
-    var div = document.createElement('div');
+    var div = document.createElement("div");
     div.className = "block";
     div.style.background = "white";
     div.style.width = ctn_width / size + "px";
@@ -17,63 +17,66 @@ function make_grid(size) {
 }
 
 function clear_grid() {
-  var blocks = document.querySelectorAll('.block'), i;
+  var blocks = document.querySelectorAll(".block"),
+    i;
   for (i = 0; i < blocks.length; ++i) {
     blocks[i].style.background = "white";
   }
 }
 
 function reset_grid() {
-  container.innerHTML = '';
+  container.innerHTML = "";
   size = prompt("Enter new grid size (up to 64).");
   if (size > 64) {
     size = 64;
-  } 
+  }
   make_grid(size);
   set_mode(mode_cur);
 }
 
 function set_mono() {
-  var blocks = document.querySelectorAll('.block'), i;
+  var blocks = document.querySelectorAll(".block"),
+    i;
   blocks.forEach(block => {
-    block.addEventListener('mouseover', function(e){
+    block.addEventListener("mouseover", function(e) {
       e.target.style.background = "black";
-    })
-  })
+    });
+  });
 }
 
 function set_rgb() {
-  var blocks = document.querySelectorAll('.block'), i;
+  var blocks = document.querySelectorAll(".block"),
+    i;
   blocks.forEach(block => {
-    block.addEventListener('mouseover', function(e){
+    block.addEventListener("mouseover", function(e) {
       var a = get_random_rgb(),
-          b = get_random_rgb(),
-          c = get_random_rgb();
-      e.target.style.background = 'rgb(' + a + ',' + b + ',' + c + ')';
-    })
-  })
+        b = get_random_rgb(),
+        c = get_random_rgb();
+      e.target.style.background = "rgb(" + a + "," + b + "," + c + ")";
+    });
+  });
 }
 
 function get_random_rgb() {
-  return Math.floor(Math.random()*255);
+  return Math.floor(Math.random() * 255);
 }
 
 function set_gray() {
-  var blocks = document.querySelectorAll('.block'), i;
+  var blocks = document.querySelectorAll(".block"),
+    i;
   blocks.forEach(block => {
     block.style.opacity = 0;
-    block.addEventListener('mouseover', function(e){
+    block.addEventListener("mouseover", function(e) {
       let op = block.style.opacity;
       if (op < 1) {
         op = parseFloat(op) + 0.1;
         block.style.opacity = op;
       }
-    })
-  })
+    });
+  });
 }
 
 function set_mode(mode) {
-  make_grid(size_cur);
   mode_cur = mode;
   switch (mode) {
     case "mono":
@@ -91,5 +94,5 @@ function set_mode(mode) {
 }
 
 // initial calling
-make_grid(size_cur);
-set_mode(mode_cur);
+ make_grid(size_cur);
+ set_mode(mode_cur);
